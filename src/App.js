a1d131feb9e78f722a840d9c606b0e36547bc5f8
natalie <nataliemae263@gmail.com>
2022-04-21 10:27:53 -0500

    finished but onChange is not working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import Counter from './Counter';
 import './App.css';
 import React from 'react';
+import CounterSlice, { counterSlice } from './features/counterSlice';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      <CounterSlice />
     </div>
   );
 }
